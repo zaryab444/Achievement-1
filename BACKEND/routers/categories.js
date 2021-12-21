676b7d98ab0b196c735e72e8f0d9fda088a52ec6
zaryab444 <shassana00@gmail.com>
2021-12-21 14:55:52 +0500

    categories put work

--- BACKEND/routers/categories.js
+++ BACKEND/routers/categories.js
@@ -2,14 +2,24 @@ const {Category} = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-
+//http://localhost:3000/api/v1/categories
 router.get(`/`, async(req , res) =>{
     const categoryList = await Category.find();
 
     if(!categoryList){
         res.status(500).json({success: false})
     }
-    res.send(categoryList)
+
+    res.status(200).send(categoryList)
+})
+
+//http://localhost:3000/api/v1/categories/id
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id);
+    if(!category){
+    return res.status(404).send('the category with the given ID cannot be created')
+    }
+    res.status(200).send(category);
 })
 
 //http://localhost:3000/api/v1/categories
@@ -31,6 +41,32 @@ router.post('/', async(req,res) =>{
 
 })
 
+
+
+//http://localhost:3000/api/v1/categories/id
+router.put('/:id', async(req,res)=>{
+  const category = await Category.findByIdAndUpdate(
+      req.params.id,
+      {
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+      },
+      {
+          new: true
+      }
+  ) 
+  if(!category)
+  return res.status(404).send('the category cannot be updated')
+
+res.send(category);
+
+})
+
+
+
+
+//http://localhost:3000/api/v1/categories/id
 router.delete('/:id', (req,res) =>{
 
     //this is the promise way logic part2 using then
