5c84cbb8a7f551cf40652b9c7b510e6c1945963a
zaryab444 <shassana00@gmail.com>
2021-12-21 12:49:52 +0500

    categories add and delete routes work

--- BACKEND/models/category.js
+++ BACKEND/models/category.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true,
+    },
+
+    color:{
+        type: String
+    },
+
+    icon:{
+        type: String
+    },
+ 
 
 })
 
--- BACKEND/routers/categories.js
+++ BACKEND/routers/categories.js
@@ -12,4 +12,39 @@ router.get(`/`, async(req , res) =>{
     res.send(categoryList)
 })
 
+//http://localhost:3000/api/v1/categories
+router.post('/', async(req,res) =>{
+     //this is the promise way logic part1 using async await
+     let category = new Category({
+         name: req.body.name,
+         icon: req.body.icon,
+         color: req.body.color
+
+     })
+
+     category = await category.save();
+
+     if(!category)
+         return res.status(404).send('the category cannot be created')
+
+     res.send(category);
+
+})
+
+router.delete('/:id', (req,res) =>{
+
+    //this is the promise way logic part2 using then
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category){
+            return res.status(200).json({success: true, message: ' the category is deleted'})
+        }
+        else{
+            return res.status(404).json({success:false, message: "Category not found"})
+        }
+    }).catch(err=>{
+        return res.status(400).json({success: false, error: err})
+    })
+})
+
+
 module.exports = router;
