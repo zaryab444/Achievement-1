0220692b84f7eaebdb643c452835844f8429348d
zaryab444 <shassana00@gmail.com>
2021-12-23 16:08:53 +0500

    user module work

--- BACKEND/models/user.js
+++ BACKEND/models/user.js
@@ -1,12 +1,49 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock:{
-        type: Number,
-        require: true
-    }
+    name: {
+
+        type: String,
+        required: true,
+    },
+    email:{
+        type: String,
+        required: true,
+    },
+    passwordHash:{
+        type: String,
+        required: true,
+    },
+    phone:{
+        type: String,
+        required: true,
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false,
+    },
+    street:{
+        type: String,
+        default: ''
+    },
+    appartment:{
+        type: String,
+        default: ''
+    },
+    zip:{
+        type: String,
+        default: ''
+    },
+    city:{
+        type: String,
+        default: ''
+    },
+    country:{
+        type: String,
+        default: ''
+    },
+    
+    
 })
 
 exports.User = mongoose.model('User', userSchema);
