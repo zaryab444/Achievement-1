ad366963f58a67d7b2d660abcfd4ce18a74ca479
zaryab444 <shassana00@gmail.com>
2021-12-23 18:57:20 +0500

    login password work

--- BACKEND/routers/users.js
+++ BACKEND/routers/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require ('jsonwebtoken');
 
 
 router.get(`/`,async(req,res) =>{
@@ -81,6 +82,31 @@ router.put('/:id', async (req,res)=>{
   res.send(user);
 })
 
+router.post('/login', async (req, res)=>{
+    const user = await User.findOne({
+       email: req.body.email 
+    })
+    const secret = process.env.secret;
+    if(!user){
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+        const token = jwt.sign(
+           {   
+               userId: user.id
+           },
+       secret,
+       {expiresIn: '1d'}
+        )
+        res.status(200).send({user: user.email , token})
+    }
+    else{
+        res.status(400).send('Password is wrong')
+    }
+
+    
+})
 
 
 module.exports = router
