fc3f18dac0c1c44fa29058320c1f13ba39bd5e4b
zaryab444 <shassana00@gmail.com>
2021-12-24 17:02:23 +0500

    user login and registration work

--- BACKEND/helpers/jwt.js
+++ BACKEND/helpers/jwt.js
@@ -2,9 +2,20 @@ const expressjwt = require('express-jwt');
 
 function authJwt(){
     const secret = process.env.secret;
+    const api = process.env.API_URL
     return expressjwt({
      secret,
      algorithms:['HS256']
+    }).unless({
+        path :[
+            //this is except api using without token
+           {url:`${api}/products`, method:['GET', 'OPTION']},
+           {url: `${api}/products/get/count`,  method:['GET', 'OPTION']},
+           {url: `${api}/categories`,  method:['GET', 'OPTION']},
+         
+           `${api}/users/register`,
+            `${api}/users/login`
+        ]
     })
 }
 
--- BACKEND/routers/users.js
+++ BACKEND/routers/users.js
@@ -102,7 +102,50 @@ router.post('/login', async (req,res) => {
 })
 
 
+router.post('/register', async (req,res)=>{
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
 
+    if(!user)
+    return res.status(400).send('the user cannot be created!')
+
+    res.send(user);
+})
+
+
+router.delete('/:id', (req, res)=>{
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user) {
+            return res.status(200).json({success: true, message: 'the user is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "user not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+router.get(`/get/count`, async (req, res) =>{
+    const userCount = await User.countDocuments((count) => count)
+
+    if(!userCount) {
+        res.status(500).json({success: false})
+    } 
+    res.send({
+        userCount: userCount
+    });
+})
 
 
 
