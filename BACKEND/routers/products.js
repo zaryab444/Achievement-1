0d7f170c41be6799ba3369e43f600e031c4854f5
zaryab444 <shassana00@gmail.com>
2022-01-03 21:17:03 +0500

    image upload works with authentication

--- BACKEND/routers/products.js
+++ BACKEND/routers/products.js
@@ -4,14 +4,26 @@ const express = require('express');
 const router  = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
+const FILE_TYPE_MAP ={
+  'image/png':'png',
+  'image/jpeg':'jpeg',
+  'image/jpg':'jpg'
+}
 
 const storage = multer.diskStorage({
   destination: function (req,file,cb){
-     cb(null, 'public/uploads')
+    const isvalid = FILE_TYPE_MAP[file.mimetype];
+    let uploadError = new Error('invalid image type');
+    if(isvalid){
+      uploadError = null
+    }
+     cb(uploadError, 'public/uploads')
   },
   filename: function (req,file,cb){
-    const fileName =  file.originalname.split(' ').join('-')
-    cb(null, `${fileName}-${Date.now()}.${mimetype}`)
+  
+    const fileName =  file.originalname.split(' ').join('-');
+    const extension = FILE_TYPE_MAP[file.mimetype];
+    cb(null, `${fileName}-${Date.now()}.${extension}`)
   }
 })
 
@@ -106,6 +118,9 @@ router.get(`/`, async(req,res)=>{
 
     if(!category) 
     return res.status(400).send('Invalid Category')
+
+    const file = req.file;
+    if(!file) return res.status(400).send('No Image in the request')
 
 
   const fileName = req.file.filename
