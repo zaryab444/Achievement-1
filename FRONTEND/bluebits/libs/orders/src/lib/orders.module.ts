8343ed20681018811ee65609bcbc29e14633782f
zaryab444 <shassana00@gmail.com>
2022-01-05 18:46:52 +0500

    adding library of shared folder

---
+++ FRONTEND/bluebits/libs/orders/src/lib/orders.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { Route, RouterModule } from '@angular/router';
+
+
+
+export const usersRoutes: Route[]=[];
+@NgModule({
+  imports: [CommonModule, RouterModule]
+})
+
+export class ordersModule{}
