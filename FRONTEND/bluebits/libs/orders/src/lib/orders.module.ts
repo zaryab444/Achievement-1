8efb118364d63ebf195c58efeb334e9fb195a1b4
zaryab444 <shassana00@gmail.com>
2022-01-05 19:04:41 +0500

    include library work

--- FRONTEND/bluebits/libs/orders/src/lib/orders.module.ts
+++ FRONTEND/bluebits/libs/orders/src/lib/orders.module.ts
@@ -2,8 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
 
-
-
 export const usersRoutes: Route[]=[];
 @NgModule({
   imports: [CommonModule, RouterModule]
---
+++ FRONTEND/bluebits/libs/ui/src/lib/slider/slider.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bluebits-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
