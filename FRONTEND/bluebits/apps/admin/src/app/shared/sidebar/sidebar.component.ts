ebaae23b12bc560a3f42a279f3ddeebb31a37b96
zaryab444 <zsohail@approcessing.com>
2022-01-31 12:24:38 +0500

    add logout functionality work

--- FRONTEND/bluebits/apps/admin/src/app/shared/sidebar/sidebar.component.ts
+++ FRONTEND/bluebits/apps/admin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '@bluebits/users';
+import { LocalstorageService } from 'libs/users/src/lib/services/localstorage.service';
+
 
 @Component({
   selector: 'admin-sidebar',
@@ -7,9 +11,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+  constructor(  private token: LocalstorageService,   private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  logoutUser() {
+    this.token.removeToken();
+    this.router.navigate(['/login'])
   }
-
 }
--- FRONTEND/bluebits/libs/users/src/index.ts
+++ FRONTEND/bluebits/libs/users/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/services/users.service';
 export * from './lib/models/user';
 export * from './lib/services/auth-guard.service';
 export * from './lib/services/jwt.interceptor';
+export * from './lib/services/auth.service';
