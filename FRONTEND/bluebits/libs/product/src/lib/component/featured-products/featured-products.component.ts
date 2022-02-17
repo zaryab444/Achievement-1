240f843392bc82e5f98d25116fa51a4bbb8e46e5
zaryab444 <zsohail@approcessing.com>
2022-02-17 15:28:05 +0500

    code updated

--- FRONTEND/bluebits/libs/product/src/lib/component/featured-products/featured-products.component.ts
+++ FRONTEND/bluebits/libs/product/src/lib/component/featured-products/featured-products.component.ts
@@ -10,27 +10,38 @@ import { ProductsService } from "../../services/products.service";
 })
 export class FeaturedProductComponent implements OnInit, OnDestroy {
 
+ // products = [];
   featuredproducts: Product[] = [];
   endSubs$ : Subject<any> = new Subject();
   constructor(private prodService: ProductsService) { }
 
   ngOnInit(): void {
     this._getFeaturedProducts();
+ //this._listProduct();
   }
 
   ngOnDestroy(): void {
       this.endSubs$.next;
       this.endSubs$.complete();
   }
-  private _getFeaturedProducts(){
+   private _getFeaturedProducts(){
 
 
-    //because i have 5 featured product in database
+  //   //because i have 5 featured product in database
     this.prodService.getFeaturedProduct(5).pipe(takeUntil(this.endSubs$)).subscribe(products => {
       this.featuredproducts =products;
     })
 
   }
+
+  //if you want to show only list without featuredd product so use this
+  // private _listProduct(){
+  //   this.prodService.getProducts().pipe(takeUntil(this.endSubs$)).subscribe(products =>{
+  //     this.products = products;
+  //   })
+  // }
+
+
 
 }
 
