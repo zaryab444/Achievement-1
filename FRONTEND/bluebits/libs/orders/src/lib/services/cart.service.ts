fef5a34b2a1822a2bb2d3826bc4198f642d3b6c9
zaryab444 <zsohail@approcessing.com>
2022-03-01 10:58:52 +0500

    cart checkout work

--- FRONTEND/bluebits/libs/orders/src/lib/services/cart.service.ts
+++ FRONTEND/bluebits/libs/orders/src/lib/services/cart.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
+import { Cart, CartItem } from "../models/cart";
 
+export const CART_KEY = "cart";
 @Injectable({
   providedIn: 'root'
 })
@@ -17,6 +19,47 @@ export class CartService {
 
     //convert json stringify for set item
     const initialCartJson = JSON.stringify(initialCart);
-    localStorage.setItem('cart', initialCartJson)
+    localStorage.setItem(CART_KEY, initialCartJson)
   }
+
+
+getCart() : Cart {
+  const cartJsonString: string = localStorage.getItem(CART_KEY)
+  const cart: Cart = JSON.parse(cartJsonString);
+  return cart
+}
+
+
+   setCartItem(cartItem: CartItem): Cart {
+    const cart = this.getCart();
+
+    //check if the product id is same in cart item then increase the quantity
+    const cartItemExist = cart.items.find((item)=> item.productId === cartItem.productId)
+    if(cartItemExist){
+        cart.items.map(item =>{
+          if(item.productId === cartItem.productId){
+            item.quantity = item.quantity + cartItem.quantity;
+            return item;
+          }
+        })
+    }
+    else{
+      cart.items.push(cartItem);
+    }
+
+
+
+
+   //convert into stringify
+   const cartJson = JSON.stringify(cart);
+
+   //set the cart item in local storage
+   localStorage.setItem(CART_KEY, cartJson)
+     return cart;
+
+   }
+
+
+
+
 }
--- FRONTEND/bluebits/libs/product/src/lib/component/product-item/product-item.component.ts
+++ FRONTEND/bluebits/libs/product/src/lib/component/product-item/product-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CartItem } from 'libs/orders/src/lib/models/cart';
+import { CartService } from 'libs/orders/src/lib/services/cart.service';
 import { Product } from '../../models/product';
 
 @Component({
@@ -11,7 +13,16 @@ import { Product } from '../../models/product';
 export class ProductItemComponent implements OnInit {
   @Input() product: Product;
 
-  constructor() {}
+  constructor(private cartService: CartService) {}
 
   ngOnInit(): void {}
+
+  addProductToCart(){
+    const cartItem: CartItem = {
+      productId: this.product.id,
+      quantity: 1
+    }
+      this.cartService.setCartItem(cartItem)
+  }
 }
+
